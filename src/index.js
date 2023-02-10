a8287aa52e8e4fac59a8211d4176c372c83f983f
JackieCisneros <krieger.jacqueline@gmail.com>
2023-02-10 14:33:27 -0700

    integrated Prestons code, wrote a little js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
+let userInput = e.target['input-text'].value;
+
+fetch(`https://images-api.nasa.gov/search?q=&{userInput}&media_type=image`)
+.then((resp) => resp.json())
+.then((data)=>{
+    generateImage(data);
+})
+
 const userName=document.querySelector('#form-div')
 const submitButton=document.querySelector('#form-button');
 
+
 submitButton.addEventListener('submit', (e)=>{
     e.preventDefault();
-    let userInput = e.target['input-text'].value;
+    
     debugger;
     fetch("http://localhost:3000", {
         method: 'POST',
