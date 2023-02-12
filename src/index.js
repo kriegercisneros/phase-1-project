227b8697b74f732d4f59eb9d1a27fb4be7623847
JackieCisneros <krieger.jacqueline@gmail.com>
2023-02-12 12:33:55 -0700

    updated js in selecting info from my api

--- src/index.js
+++ src/index.js
@@ -7,11 +7,15 @@ let h3 = document.querySelector('#some-display-h3');
 let p = document.querySelector('#description-display-p');
 
 //selects for user generated data
-let userInput = searchInput.value
-const searchSubmit = document.querySelector('#search-submit');
 
-//simply a reference to the collected info from our API
-    fetch('https://images-api.nasa.gov/search?q=nebula&media_type=image')
+const searchSubmit = document.querySelector('#search-bar');
+
+
+searchSubmit.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    let userInput = searchInput.value;
+    
+     fetch(`https://images-api.nasa.gov/search?q=${userInput}&media_type=image`)
     .then((r)=>r.json())
     .then((data)=>{ 
         let array = data.collection.items;//an array of objects
@@ -20,47 +24,21 @@ const searchSubmit = document.querySelector('#search-submit');
         let randomDescription = randomObject.data[0].description;
         let randomTitle = randomObject.data[0].title;
         let randomKeywords = randomObject.data[0].keywords
-        console.log(randomDescription);
-        console.log(randomImage);
-        console.log(randomTitle);
-        console.log(randomKeywords);
-        console.log(data)
-
+        // console.log(randomDescription);
+        // console.log(randomImage);
+        // console.log(randomTitle);
+        // console.log(randomKeywords);
     })
+})
+
+
+
 
 //variables to declare submit button and user name form for future username
 // const userName=document.querySelector('#form-div')
 // const submitButton=document.querySelector('#form-button');
 
-//eventlistener for submit inquiry event
-// searchSubmit.addEventListener('submit', (event)=>{
-//     event.preventDeault();
-// })
-  
 
-
-function generateRandomImage(info){
-    let array = info.collection.items
-    let randomItem = array[Math.floor(Math.random()*array.length)];
-
-    image.src = randomItem.links[0]
-    // console.log('hello')
-}
-
-// submitButton.addEventListener('submit', (e)=>{
-//     e.preventDefault();
-//     fetch("http://localhost:3000", {
-//         method: 'POST',
-//         headers: {
-//             'content-Type': 'application/json'
-//         },
-//         body: JSON.stringify({
-//             user : userInput
-//         })
-//     })
-//     .then(response => response.json())
-//     .then(data => console.log(data)) 
-// })
 
 
 
